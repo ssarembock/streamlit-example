ef05532e14b6b9edf58c599030d12ea67b3ee66e
ssarembock <ssarembock@gmail.com>
2022-07-14 14:36:11 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,17 +6,14 @@ import streamlit as st
 import pandas as pd
 
 """
-# Welcome to Streamlit!
+# Decision
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
 
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
-
-In the meantime, below is an example of what you can do with just a few lines of code:
 """
 cards = [str(k) for k in range(2,9)] + ["A","K","Q","J","T"]
 suits = ["o","s"]
+
+st.sidebar.title("All In Or Fold")
 
 card_1 = st.sidebar.radio("Pick First Card",cards)
 card_2 = st.sidebar.radio("Pick Second Card",cards)
